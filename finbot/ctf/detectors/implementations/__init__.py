f09f5095f597e57c7268e34b34466033479acf81
Venkata Sai Kishore Modalavalasa <sai.kishu@gmail.com>
2026-03-01 11:09:49 -0800

    Policy Bypass - Non Compliant Vendor Challenge

--- finbot/ctf/detectors/implementations/__init__.py
+++ finbot/ctf/detectors/implementations/__init__.py
@@ -1,8 +1,11 @@
 """Detector Implementations"""
 
 # Imports trigger registration via decorators
+from finbot.ctf.detectors.implementations.policy_bypass_non_compliant import (
+    PolicyBypassNonCompliantDetector,
+)
 from finbot.ctf.detectors.implementations.system_prompt_leak import (
     SystemPromptLeakDetector,
 )
 
-__all__ = ["SystemPromptLeakDetector"]
+__all__ = ["PolicyBypassNonCompliantDetector", "SystemPromptLeakDetector"]
